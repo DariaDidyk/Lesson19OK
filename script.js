01cff3540f0c8f164e4d41b24799c973fdae6f5b
dasha <dashka.didyk@gmail.com>
2020-09-07 21:24:03 +0300

    TODOandHW

---
+++ script.js
@@ -0,0 +1,42 @@
+// const units = [new Unit('Virus', 200, 500)]
+
+// units.push(units[0].clone());
+
+// // console.log(units);
+
+
+// const squad = new Squad(units);
+
+// console.log(squad);
+
+
+
+// our HW
+
+
+
+// function MilitaryResource(type, health, distance) {
+//     this.type = type;
+//     this.health = health;
+//     this.maxHealth = maxHealth;
+//     this.distance = distance;
+//     this.maxDistance = maxDistance;
+//  }
+ 
+//  MilitaryResource.prototype.isReadyToMove;
+//  MilitaryResource.prototype.isReadyToFight;
+//  MilitaryResource.prototype.restore;
+//  MilitaryResource.prototype.clone;
+ 
+ 
+//  function Squad(defaultResources) {
+//     this.squad = [];
+//     if (defaultResources) this.combineResources(defaultResources);
+//  }
+ 
+//  Squad.prototype.isReadyToMove;
+//  Squad.prototype.isReadyToFight;
+//  Squad.prototype.restore;
+//  Squad.prototype.getReadyToMoveResources;
+//  Squad.prototype.combineResources;
+//  Squad.prototype.cloneResource;
